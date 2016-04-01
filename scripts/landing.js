65b0a54eefafac62872a5300be67e043eca56c7a
Chris <chris@chrisgillisdesign.com>
2016-03-31 20:36:49 -0400

    fixing animation, collections

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< 70a4c8b4c65b2f83982ebee2ea9be521c5d086d5
->>>>>>> master
- var pointsArray = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
  var animatePoints = function(points) {
                     
     var revealPoint = function(index) {
@@ -33,44 +29,4 @@ window.onload = function() {
              animatePoints(pointsArray);   
          }
      });
-<<<<<<< HEAD
  }
-=======
- }
-=======
-                var animatePoints = function() {
- 
-                 var points = document.getElementsByClassName('point');
- 
-                 var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-                 };
-                   
-                
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();
- 
-             };
-       
-          
->>>>>>> First commit for gh-pages
->>>>>>> master
